ddc90ea69bacc640a2189ca13da5333efac7f83b
mdflynn <mdflynn34@outlook.com>
2020-09-12 18:21:28 -0600

    Format countdown function

--- src/activity.js
+++ src/activity.js
@@ -8,22 +8,21 @@ class Activity {
     this.completed = false;
   }
   countdown() {
-    var countDownDate = new Date().getTime() + (this.minutes * 60 * 1000) + (this.seconds * 1000) + 1000;
+    var countDownDate = new Date().getTime() + (this.minutes * 60 * 1000) + (this.seconds * 1000);
     var timeConvert = setInterval(function() {
-    var now = new Date().getTime();
-    var remainingTime = countDownDate - now;
-    var minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
-    var addZeroMin = minutes < 10 ? "0" + minutes : minutes;
-    var addZeroSec = seconds < 10 ? "0" + seconds : seconds;
-    document.querySelector('h1').innerHTML = `${addZeroMin}:${addZeroSec}`;
-    if (distance < 0) {
-      clearInterval(timeConvert);
-      document.querySelector('h1').innerHTML = "EXPIRED";
-      //add alert
-    }
-  }, 1000);
-
+      var now = new Date().getTime();
+      var remainingTime = countDownDate - now;
+      var minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+      var addZeroMin = minutes < 10 ? "0" + minutes : minutes;
+      var addZeroSec = seconds < 10 ? "0" + seconds : seconds;
+      document.querySelector('h1').innerHTML = `${addZeroMin}:${addZeroSec}`;
+      if (remainingTime < 0) {
+        clearInterval(timeConvert);
+        document.querySelector('h1').innerHTML = "EXPIRED";
+        alert `Time's up! Nice work!`;
+      }
+    }, 1000);
   }
 
   markComplete() {
